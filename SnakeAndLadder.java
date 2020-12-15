ce13a87b76ecd900683669dceb091df7a849b38a
gautamb97 <www.gautam971997@gmail.com>
2020-12-15 19:35:37 +0530

    Added:options_noplay_snake_and_ladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,32 @@
 public class SnakeAndLadder {
     public static void main(String [] args){
         System.out.println("Welcome to Snake and Ladder Game");
+        Player choice = new Player();
+        choice.chooseOption();
     }
 }
 class Player{
     int playerPosition = 0;
 
-    double die = Math.floor(Math.random() * 10 ) % 6;
+    double die = Math.floor(Math.random() * 10 ) % 7;
+
+    public void chooseOption(){
+
+        int choose = (int) Math.floor(Math.random() * 10 ) % 3;
+        switch(choose){
+            case 0:
+                System.out.println("No Play");
+                System.out.println(playerPosition);
+                break;
+            case 1:
+                System.out.println("Yay!! I got ladder");
+                playerPosition +=die;
+                break;
+            default:
+                System.out.println("oops!! snake killed me");
+                playerPosition -=die;
+                break;
+        }
+        System.out.println("Player position "+playerPosition);
+    }
 }
