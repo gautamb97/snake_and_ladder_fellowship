dea1499920db17b260f6bbccbdae29e62c7b6d98
gautamb97 <www.gautam971997@gmail.com>
2020-12-16 09:20:12 +0530

    Added:winning_point

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,26 +7,31 @@ public class SnakeAndLadder {
 }
 class Player{
     int playerPosition = 0;
+    private final int winning_point = 100;
 
-    double die = Math.floor(Math.random() * 10 ) % 7;
+    double die = 1+(Math.floor(Math.random() * 10 ) % 6);
 
     public void chooseOption(){
-
-        int choose = (int) Math.floor(Math.random() * 10 ) % 3;
-        switch(choose){
-            case 0:
-                System.out.println("No Play");
-                System.out.println(playerPosition);
-                break;
-            case 1:
-                System.out.println("Yay!! I got ladder");
-                playerPosition +=die;
-                break;
-            default:
-                System.out.println("oops!! snake killed me");
-                playerPosition -=die;
-                break;
+        while(playerPosition<winning_point) {
+            if (playerPosition < 0)
+                playerPosition = 0;
+            int choose = (int) Math.floor(Math.random() * 10) % 3;
+            switch (choose) {
+                case 0:
+                    System.out.println("No Play");
+                    break;
+                case 1:
+                    System.out.println("Yay!! I got ladder");
+                    playerPosition += die;
+                    break;
+                default:
+                    System.out.println("oops!! snake killed me");
+                    playerPosition -= die;
+                    break;
+            }
+            System.out.println("Die Value: "+die);
         }
-        System.out.println("Player position "+playerPosition);
+        System.out.println("Player position " + playerPosition);
+        System.out.println("Hip....Hip...Hurrey....I won!!!");
     }
 }
